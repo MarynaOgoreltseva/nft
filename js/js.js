c0626449291327ba6139bf1db2f34648c2313928
MarynaOgoreltseva <ogoreltseva1@gmail.com>
2023-11-30 12:04:53 +0200

    gallery close correct

--- js/js.js
+++ js/js.js
@@ -15,7 +15,8 @@ oGallery.addEventListener('click', function(ev) {
   if (ev.target.tagName != 'IMG') { return false; };
   if (ev.target.className === 'user__photo') { return false; };
   let oTarget = ev.target, nWidth, nHeight, nRatio = oTarget.offsetWidth / oTarget.offsetHeight;
-  let oBig = this.appendChild(document.createElement('DIV'));
+    let oBig = this.appendChild(document.createElement('DIV'));
+    oBig.classList.add('showImg')
   oBig.style.position = `absolute`;
   oBig.style.top = `${oTarget.offsetTop}px`; oBig.style.left = `${oTarget.offsetLeft}px`;
   oBig.style.width = `${oTarget.offsetWidth}px`; oBig.style.height = `${oTarget.offsetHeight}px`;
@@ -25,16 +26,17 @@ oGallery.addEventListener('click', function(ev) {
     nWidth = this.offsetWidth; nHeight = nWidth / nRatio;
   };
   oBig.style.background = `center / 100% 100% no-repeat url('${oTarget.currentSrc}')`;
-  oBig.insertAdjacentHTML('beforeend', '<div class="close"></div>');
-  oBig.addEventListener('transitionend', function() { this.querySelector('.close').style.opacity = 1; });
-  oBig.addEventListener('click', function(ev) {
-    ev.stopPropagation();
-    this.addEventListener('transitionend', function() { this.remove(); });
-    this.style.transition = `.5s ease-in`;
-    this.style.height = this.style.width = `0px`;
-    $('.overlay').toggleClass('is-gallery');
-  oGallery.classList.toggle('show', false);
-  });
+  oBig.insertAdjacentHTML('afterend', '<div class="close"></div>');
+    oBig.addEventListener('transitionend', function () { document.querySelector('.close').style.opacity = 1; });
+    $('.close,.showImg').on('click', function (ev) {
+        ev.stopPropagation();
+        this.addEventListener('transitionend', function() { this.remove(); });
+        this.style.transition = `.1s ease-in`;
+        this.style.height = this.style.width = `0px`;
+        $('.overlay').toggleClass('is-gallery');
+        oGallery.classList.toggle('show', false);
+        $('.showImg,.close').remove()
+    })
   oBig.classList.toggle('active');
   oBig.style.width = `${nWidth / 1.3}px`; oBig.style.height = `${nHeight / 1.3}px`;
   oBig.style.top = `40%`;
